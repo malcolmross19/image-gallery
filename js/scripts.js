8e174a17e7986d88339b1a3707e7ba7e9637966a
Malcolm Ross <MJRoss@fullsail.edu>
2017-10-20 21:55:23 -0400

    Changed api end point to retrieve 9 results per page. Added variables for capturing the travel header an globe image. Added additional lines to pull in additional image urls. Added event listener to animate globe image.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,8 @@
 const apiEndPoint =
-    "https://api.unsplash.com/search/photos?page=1&query=travel&client_id=628a9768332e7da922f8b0e4748f0d7f66139de9a0a1ea55b244cfd1d562fdf5";
+    "https://api.unsplash.com/search/photos?page=1&query=travel&per_page=9&client_id=628a9768332e7da922f8b0e4748f0d7f66139de9a0a1ea55b244cfd1d562fdf5";
 let galleryContent = '';
+let travel = document.querySelector('#travelHeader');
+let globe = document.querySelector('#globe');
 
 // Event Listeners
 window.addEventListener('load', function() {
@@ -21,16 +23,18 @@ window.addEventListener('load', function() {
             galleryContent += '<article class=picture>';
             galleryContent += '<img src=' + responseObject.results[i].urls.regular + ' ';
             galleryContent += 'srcset="' + responseObject.results[i].urls.regular + ', ';
-            galleryContent += responseObject.results[i].urls.regular + '" ';
+            galleryContent += responseObject.results[i].urls.small + ', ';
+            galleryContent += responseObject.results[i].urls.thumb + '" ';
             galleryContent += 'sizes="(max-width:300px) 100vw,';
             galleryContent += '(max-width:600px) 33vw,';
+            galleryContent += '(max-width:800px) 33vw';
             galleryContent += '300px" ';
             galleryContent += 'alt="' + responseObject.results[i].description + '" />';
             galleryContent += '<div class=overlay>';
-            galleryContent += '<img src="images/user-icon.svg" alt="user icon" class="user" /><p class="user">';
-            galleryContent += responseObject.results[i].user.name + '</p>';
-            galleryContent += '<img src="images/heart.svg" alt="likes" class="likes" /><p class=likes>';
-            galleryContent += responseObject.results[i].likes + '</p>';
+            galleryContent += '<div class="user"><img src="images/user-icon.svg" alt="user icon" /><p>';
+            galleryContent += responseObject.results[i].user.name + '</p></div>';
+            galleryContent += '<div class="likes"><img src="images/heart.svg" alt="likes" /><p>';
+            galleryContent += responseObject.results[i].likes + '</p></div>';
             galleryContent += '</div>';
             galleryContent += '</article>';
         }
@@ -44,4 +48,14 @@ window.addEventListener('load', function() {
         gallery.innerHTML = galleryContent;
     }
 
+}, false);
+
+travel.addEventListener('click', function() {
+    let globeClasses = globe.classList;
+
+    if(globeClasses.contains('rotate')){
+        globeClasses.remove('rotate');
+    } else {
+        globeClasses.add('rotate');
+    }
 }, false);
